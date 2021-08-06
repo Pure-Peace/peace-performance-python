1b80965226e735ad16e1b3fe9aef6eb2564c2aca
Pure-Peace <940857703@qq.com>
2021-08-06 15:42:53 +0800

    add: class Beatmap

--- peace_performance_python/beatmap.py
+++ peace_performance_python/beatmap.py
@@ -1,17 +1,67 @@
-from typing import Any
+from typing import Any, Optional
+from pathlib import Path
+
 from ._peace_performance import pp as _p
 
-from .common import asyncinit
+
+RawBeatmap = object
 
 
-@asyncinit
 class Beatmap:
-    async def __init__(self, osu_file_path: str) -> 'Beatmap':
-        self._b = await raw_read_beatmap(osu_file_path)
+    '''
+    The beatmap used to calculate the pp
+
+    ### Examples:
+    ```
+    # Read and parse .osu files from local
+    beatmap = await Beatmap('path_to_osu_file')
+    # Same as
+    beatmap = await Beatmap.create('path_to_osu_file')
+
+    # We can reload this .osu files as:
+    await beatmap
+    # or
+    await beatmap.reload()
+    
+    # We can load another .osu files as:
+    await beatmap.async_init('path_to_another_osu_file')
+
+    ```
+    '''
+    _raw: Optional['RawBeatmap']
+    path: Path
+
+    def __init__(self, osu_file_path: Path):
+        '''Init async with .osu files'''
+        self.path = osu_file_path
+
+    def __await__(self):
+        return self.reload(self.path).__await__()
+
+    async def reload(self) -> 'Beatmap':
+        '''Reload this .osu files'''
+        self._raw = await raw_read_beatmap(self.path)
+        return self
+
+    async def async_init(self, osu_file_path: Path) -> 'Beatmap':
+        '''Load the .osu files with path'''
+        self.path = osu_file_path
+        self._raw = await raw_read_beatmap(self.path)
+        return self
+
+    @classmethod
+    async def create(cls, osu_file_path: Path) -> 'Beatmap':
+        '''Init async with .osu files'''
+        obj = cls(osu_file_path)
+        await obj.async_init(osu_file_path)
+        return obj
+
+    @property
+    def is_initialed(self) -> bool:
+        '''Returns whether the beatmap (.osu files) has been loaded and parsed'''
+        return bool(self._raw)
 
 
-async def raw_read_beatmap(osu_file_path: str) -> Any:
-    '''
-    Read and parse .osu files from local
-    '''
+async def raw_read_beatmap(osu_file_path: Path) -> RawBeatmap:
+    '''Read and parse .osu files from local'''
     return await _p.read_beatmap(osu_file_path)
