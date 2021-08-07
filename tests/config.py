d9872d70f76e0417c4ea6f5cb233697ec8746675
Pure-Peace <940857703@qq.com>
2021-08-07 23:12:58 +0800

    add: test beatmaps

--- tests/config.py
+++ tests/config.py
@@ -1,5 +1,12 @@
-BEATMAP_DIR = r'D:/PurePeace/Desktop/osudev/pp-server/example_data/beatmaps/'
-TEST_BEATMAP_FILE = r'eae63c5e82313da9c08f7565f6b6505b.osu'
+BEATMAP_DIR = r'./test_beatmaps/'
+
+# Test beatmaps
+PADORU = r'padoru.osu' # super short - 5kb
+HITORIGOTO = r'hitorigoto.osu'  # short - 15kb
+FREEDOM_DIVE = r'freedom_dive.osu'  # stream medium - 50kb
+SOTARKS = r'sotarks.osu'  # jump medium - 68kb
+GALAXY_BURST = r'galaxy_burst.osu'  # tech - 102kb
+UNFORGIVING = r'unforgiving.osu'  # marathon - 238kb
 
 
 def join_beatmap(beatmap: str) -> str:
