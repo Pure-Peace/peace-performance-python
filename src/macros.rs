d404e278539033a85f4877b5160b3cd78467696b
Pure-Peace <940857703@qq.com>
2021-08-10 01:00:37 +0800

    fix: #[getter]

--- src/macros.rs
+++ src/macros.rs
@@ -1,21 +1,21 @@
 #[macro_export]
 macro_rules! pyo3_getters_generator {
     ($type:ident, {$($attr:ident: $returns:ident),*}, impl {$($others:item)*}) => {
-        #[pymethods] impl $type {$(pub fn $attr(&self) -> $returns {self.0.$attr})* $($others)*}
+        #[pymethods] impl $type {$(#[getter]pub fn $attr(&self) -> $returns {self.0.$attr})* $($others)*}
     };
     ($type:ident, {$($attr:ident: $returns:ident),*}) => {
-        #[pymethods] impl $type {$(pub fn $attr(&self) -> $returns {self.0.$attr})*}
+        #[pymethods] impl $type {$(#[getter]pub fn $attr(&self) -> $returns {self.0.$attr})*}
     };
     ($type:ident, {$($attr:ident: $returns:ident),*}; fn {$( $func:ident: $ret:ident),*}) => {
         #[pymethods] impl $type {
-            $(pub fn $attr(&self) -> $returns {self.0.$attr})*
-            $(pub fn $func(&self) -> $ret {self.0.$func()})*
+            $(#[getter]pub fn $attr(&self) -> $returns {self.0.$attr})*
+            $(#[getter]pub fn $func(&self) -> $ret {self.0.$func()})*
         }
     };
     ($type:ident, {$($attr:ident: $returns:ident),*}; fn {$( $func:ident: $ret:ident),*}; impl {$($others:item)*}) => {
         #[pymethods] impl $type {
-            $(pub fn $attr(&self) -> $returns {self.0.$attr})*
-            $(pub fn $func(&self) -> $ret {self.0.$func()})*
+            $(#[getter]pub fn $attr(&self) -> $returns {self.0.$attr})*
+            $(#[getter]pub fn $func(&self) -> $ret {self.0.$func()})*
             $($others)*
         }
     };
