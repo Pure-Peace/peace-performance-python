a11723f1d06726c94533a6332d6ae6bc59fdefc4
Pure-Peace <940857703@qq.com>
2021-08-13 15:32:38 +0800

    Publish: v1.0.1

--- peace_performance_python/__init__.py
+++ peace_performance_python/__init__.py
@@ -1,3 +1,14 @@
+'''
+:copyright: (C) 2021 Pure-Peace
+:license: MIT, see LICENSE for details.
+'''
+
+__title__ = 'Pure-Peace'
+__author__ = 'Pure-Peace'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2021 Pure-Peace'
+__version__ = '1.0.1'
+
 from .objects import *
 from . import types
 from . import functions
