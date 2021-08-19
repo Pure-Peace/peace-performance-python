dd694e9cbb76d1eb1ecab298f85c73044ea69494
Pure-Peace <940857703@qq.com>
2021-08-19 18:15:39 +0800

    add: optimization

--- src/macros.rs
+++ src/macros.rs
@@ -59,8 +59,12 @@ macro_rules! pyo3_set_sys_modules {
     ($m:ident, $py:ident; {$($module:expr),*}) => {
         let sys = PyModule::import($py, "sys")?;
         let sys_modules: &PyDict = sys.getattr("modules")?.downcast()?;
-        let name = $m.name()?.split_once('.').map(|s| format!("{}.", s.0)).unwrap_or("".into());
-        $(sys_modules.set_item(format!("{}_peace_performance.{}", name, $module), $m.getattr($module)?)?;)*
+        let name = $m.name()?.split_once('.').map(|s| s.0);
+        $(sys_modules.set_item(if name.is_none() {
+            format!("_peace_performance.{}", $module)
+        } else {
+            format!("{}._peace_performance.{}", name.unwrap(), $module)
+        }, $m.getattr($module)?)?;)*
     };
 }
 
